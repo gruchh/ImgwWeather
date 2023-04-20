2fba0bf4e79d9f5078f92376c2e5bae4f9188db6
Kuba <kubol2@gmail.com>
2023-04-20 14:08:58 +0200

    Measure Controller with Service & Repository implementation #7

--- src/main/java/com/gruchh/weather/App/Controller/WaterMeasureController.java
+++ src/main/java/com/gruchh/weather/App/Controller/WaterMeasureController.java
@@ -5,11 +5,9 @@ import com.gruchh.weather.App.Service.WaterMeasureServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
@@ -20,10 +18,22 @@ public class WaterMeasureController {
 
     private final WaterMeasureServiceImpl waterMeasureService;
 
+    @PostMapping()
+    public ResponseEntity<WaterMeasureDto> createWaterMeasure(
+            @Valid @RequestBody WaterMeasureDto createWaterMeasureDto) {
+        WaterMeasureDto waterMeasureDto = waterMeasureService.createMeasure(createWaterMeasureDto);
+        return new ResponseEntity<>(waterMeasureDto, HttpStatus.CREATED);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<WaterMeasureDto> findWaterMeasureById(@Min(1) @NotNull @PathVariable Long id) {
-
         WaterMeasureDto waterMeasureDto = waterMeasureService.findById(id);
         return new ResponseEntity<>(waterMeasureDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWalletById(@Min(1) @NotNull @PathVariable Long id) {
+        waterMeasureService.deleteMeasureById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
